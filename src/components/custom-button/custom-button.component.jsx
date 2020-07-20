b16858b2a9e8b1d27eb3b3e9d6d187d8ff64f449
IbrahimHasan <52077209+ibrahimHasa@users.noreply.github.com>
2020-07-20 10:58:03 +0200

    new shop data, update cartutils

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './custom-button.styles.scss';
+import React from "react";
+import "./custom-button.styles.scss";
 
-export default function CusttomButton({ children, isGoogleSignIn, ...otherProps }) {
-    return (
-        <button className={` ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button  `} {...otherProps}>
-            {children}
-        </button>
-    )
+export default function CusttomButton({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) {
+  return (
+    <button
+      className={`
+      ${inverted ? "inverted" : ""}
+         ${isGoogleSignIn ? "google-sign-in" : ""} custom-button  `}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 }
